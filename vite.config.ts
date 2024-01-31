ce998263a094cf6327561cd12668b9a68e875424
shaohui_jin <1051131737@qq.com>
2024-01-31 14:54:20 +0800

    调整elementPlus的按需引入，页面title

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
@@ -9,6 +12,12 @@ export default defineConfig({
       script: {
         defineModel: true
       }
+    }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
     })
   ],
   resolve: {
